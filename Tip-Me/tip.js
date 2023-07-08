b0f9b66008602c1d0915c66af56272b410eb8c08
royalrock11 <royalrock13@gmail.com>
2023-07-08 18:06:34 -0400

    First Commit

---
+++ Tip-Me/tip.js
@@ -0,0 +1,39 @@
+
+const web3 = new Web3(Web3.givenProvider)
+const form = document.querySelector("form");
+
+
+const send = async function (amount) {
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+const wei = web3.utils.toWei(amount, "ether")
+
+  if (accounts.length > 0) {
+    window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [{
+        from: accounts[0],
+        to: "0xBcf12550c5A32AB90415495358388F3C1707CEE3",
+        value: web3.utils.toHex(wei)
+      }]
+    })
+  }
+};
+
+if (window.ethereum) {
+  form.classList.add("has-eth");
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (window.ethereum) {
+    const input = form.querySelector("input");
+    send(input.value);
+  } else {
+    alert("Please install MetaMask to use this dApp");
+  }
+});
+ 
